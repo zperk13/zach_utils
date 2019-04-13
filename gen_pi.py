853f05be7f3d1f40334b40c18711422cc235fd2d
BuildTools <unconfigured@null.spigotmc.org>
2019-04-13 10:49:42 -0500

    Made progress bar for Gregory_Leibniz optional. Used TimeTracker module for tracking time instead in if __name__ == '__main__': along with adding 1,000,000 to the functions.

--- gen_pi.py
+++ gen_pi.py
@@ -1,21 +1,35 @@
 # Different ways of generating pi
 
-from tqdm import trange
 import math
-import time
+
+from tqdm import trange
+
+from zach_utils.TimeTracker import TimeTracker
 
 
-def Gregory_Leibniz(max_num):
-    pi = 4 / 1
-    add = False
-    for x in trange(3, max_num + 1, 2, desc='Gregory_Leibniz calculation'):
-        if add:
-            pi += 4 / x
-            add = False
-        else:
-            pi -= 4 / x
-            add = True
-    return pi
+def Gregory_Leibniz(max_num, progress_bar=False):
+    if progress_bar:
+        pi = 4 / 1
+        add = False
+        for x in trange(3, max_num + 1, 2, desc='Gregory_Leibniz calculation'):
+            if add:
+                pi += 4 / x
+                add = False
+            else:
+                pi -= 4 / x
+                add = True
+        return pi
+    else:
+        pi = 4 / 1
+        add = False
+        for x in range(3, max_num + 1, 2):
+            if add:
+                pi += 4 / x
+                add = False
+            else:
+                pi -= 4 / x
+                add = True
+        return pi
 
 
 def Nilakantha(max_num):
@@ -43,11 +57,11 @@ def limit(max_num):
 
 
 if __name__ == '__main__':
-    gl_var = Gregory_Leibniz()
-    time1 = time.time()
-    n_var = Nilakantha()
-    time2 = time.time()
-    l_var = limit()
+    TimeTracker()
+    gl_var = Gregory_Leibniz(1_000_000)
+    n_var = Nilakantha(1_000_000)
+    l_var = limit(1_000_000)
+    print(TimeTracker.stop())
     print('Gregory_Leibniz:', gl_var)
     print('Nilakantha:', n_var)
     print('Limit:', l_var)
