9acc9b46577f5c49e74a9b933556d96e77977d2f
BuildTools <unconfigured@null.spigotmc.org>
2019-04-13 21:40:05 -0500

    made drop_character more efficient

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,7 +1,3 @@
 def drop_character(str, character):
     # Removes all of a certain character from string
-    list_var = str.split(character)
-    str_var = ''
-    for item in list_var:
-        str_var += item
-    return str_var
+    return str.replace(character, '')
