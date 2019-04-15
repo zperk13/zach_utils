a886fc4e83282b2103809b12ab6ab1293b90fc0e
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 13:00:33 -0500

    made new function, no_double_lines

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,3 +1,12 @@
 def drop_character(str, character):
     # Removes all of a certain character from string
     return str.replace(character, '')
+
+
+def no_double_lines(string):
+    while True:
+        if '\n\n' in string:
+            string = string.replace('\n\n', '\n')
+            print(string.count('\n'))
+        else:
+            return string
