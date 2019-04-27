d5c4f7c57c368963da3e181ff6effe0848d38e2b
BuildTools <unconfigured@null.spigotmc.org>
2019-04-26 22:39:48 -0500

    fixed bug where read_pickle wouldn't return value

--- README.md
+++
@@ -1,1 +0,0 @@
-# zach_utils
--- easy_pickles.py
+++ easy_pickles.py
@@ -18,10 +18,10 @@ def read_pickle(filename):
     if len(filename) > 7:
         if filename[-7:] == '.pickle':
             with open(filename, 'rb') as f:
-                pickle.load(f)
+                return pickle.load(f)
         else:
             with open(filename + '.pickle', 'rb') as f:
-                pickle.load(f)
+                return pickle.load(f)
     else:
         with open(filename + '.pickle', 'rb') as f:
-            pickle.load(f)
+            return pickle.load(f)
